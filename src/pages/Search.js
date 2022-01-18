78d985eb6e81815975c091335b08f646419a4918
Juliane Alves <julianeju3654@gmail.com>
2022-01-18 10:40:11 -0300

    Tentando novamente os requisitos

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,52 @@
 import React from 'react';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchArtBand: '',
+      searchButtonDisabled: true,
+    };
+  }
+
+  handleInputChange = ({ target: { value } }) => {
+    const CARACTERS_MIN = 2;
+    const habiliteSearchButton = value.length >= CARACTERS_MIN;
+    this.setState({
+      searchArtBand: value,
+      searchButtonDisabled: !habiliteSearchButton,
+    });
+  }
+
   render() {
+    const { searchArtBand, searchButtonDisabled } = this.state;
     return (
-      <div data-testid="page-search" />
+      <div data-testid="page-search">
+        <form>
+          <label htmlFor="searchArtBand">
+            <input
+              type="text"
+              name="searchArtBand"
+              placeholder="Insira nome da banda ou artista"
+              data-testid="search-artist-input"
+              id="user"
+              value={ searchArtBand }
+              // onChange={ this.handleInputChange }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            onClick={ this.handleClick }
+            disabled={ searchButtonDisabled }
+          >
+            Pesquisar
+          </button>
+          { /* operador ternario */ }
+          { /* loading ? <p>Carregando...</p> : null */ }
+          { /* redirectEnable ? <Redirect to="/search" /> : null */ }
+        </form>
+      </div>
     );
   }
 }
