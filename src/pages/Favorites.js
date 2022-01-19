e6d0b893122f8dca4b2d00b5ce8e30f022d4d842
Juliane Alves <julianeju3654@gmail.com>
2022-01-19 09:16:25 -0300

    Tentando resolver bugs

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -4,7 +4,7 @@ import Header from '../components/Header';
 class Favorites extends React.Component {
   render() {
     return (
-      <div data-testid="page-album">
+      <div data-testid="page-favorites">
         <Header />
       </div>
     );
